495ba50bc534da670bd4c92e5017de9350cf6003
apraba <39337603+apraba@users.noreply.github.com>
2018-05-22 15:32:48 -0300

    Add files via upload

---
+++ cambio_luminarias_v1/data/Recambiodeluminarias_3.js
@@ -0,0 +1,15 @@
+var json_Recambiodeluminarias_3 = {
+"type": "FeatureCollection",
+"name": "Recambiodeluminarias_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "q2wHide_Cantidadd": "7911", "Identifica": "Villa 21-24", "cant_d_str": "7.911", "pot_d_str": "300,60", "pot_a_str": "112.731,80", "equ_str": "33,80" }, "geometry": { "type": "Point", "coordinates": [ -58.400111109355663, -34.656277781327489 ] } },
+{ "type": "Feature", "properties": { "q2wHide_Cantidadd": "6961", "Identifica": "Villa 20", "cant_d_str": "6.961", "pot_d_str": "264,50", "pot_a_str": "140.194,50", "equ_str": "42,00" }, "geometry": { "type": "Point", "coordinates": [ -58.466611109380992, -34.674888891297066 ] } },
+{ "type": "Feature", "properties": { "q2wHide_Cantidadd": "4797", "Identifica": "Villa 31", "cant_d_str": "4.797", "pot_d_str": "182,30", "pot_a_str": "96.611,60", "equ_str": "29,00" }, "geometry": { "type": "Point", "coordinates": [ -58.378166669344026, -34.584027781344609 ] } },
+{ "type": "Feature", "properties": { "q2wHide_Cantidadd": "2560", "Identifica": "Villa 1-11-14", "cant_d_str": "2.560", "pot_d_str": "97,30", "pot_a_str": "29.670,40", "equ_str": "8,90" }, "geometry": { "type": "Point", "coordinates": [ -58.440805559369934, -34.648694441311392 ] } },
+{ "type": "Feature", "properties": { "q2wHide_Cantidadd": "4637", "Identifica": "Villa 15 - Ciudad Oculta", "cant_d_str": "4.637", "pot_d_str": "176,20", "pot_a_str": "35.241,20", "equ_str": "10,60" }, "geometry": { "type": "Point", "coordinates": [ -58.490638889390112, -34.672638891286518 ] } },
+{ "type": "Feature", "properties": { "q2wHide_Cantidadd": "1797", "Identifica": "Villa 3 - Fatima", "cant_d_str": "1.797", "pot_d_str": "68,30", "pot_a_str": "9.560,00", "equ_str": "2,90" }, "geometry": { "type": "Point", "coordinates": [ -58.454722219375576, -34.657555561304349 ] } },
+{ "type": "Feature", "properties": { "q2wHide_Cantidadd": "808", "Identifica": "Villa Rodrigo Bueno", "cant_d_str": "808", "pot_d_str": "30,70", "pot_a_str": "10.439,40", "equ_str": "3,10" }, "geometry": { "type": "Point", "coordinates": [ -58.35469443933799, -34.618111111349769 ] } },
+{ "type": "Feature", "properties": { "q2wHide_Cantidadd": "642", "Identifica": "Villa 6 - Cildañez", "cant_d_str": "642", "pot_d_str": "24,40", "pot_a_str": "3.415,40", "equ_str": "1,00" }, "geometry": { "type": "Point", "coordinates": [ -58.472277779382047, -34.654027781297032 ] } }
+]
+}
